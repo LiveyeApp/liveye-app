850084fb35ef7f70a78dc0d5cd150e78dd85c634
MosheOgalbo <65408488+MosheOgalbo@users.noreply.github.com>
2024-11-10 03:09:04 +0200

    add page login

--- FunGo-app/app/(auth)/_layout.tsx
+++ FunGo-app/app/(auth)/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="sign-up/index" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in/index" options={{ headerShown: false }} />
+      <Stack.Screen name="(sign-up)" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
   );
 };
--- FunGo-app/app/index.tsx
+++ FunGo-app/app/index.tsx
@@ -1,36 +1,8 @@
-import { View, Text ,StatusBar} from 'react-native';
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { Redirect } from "expo-router";
 
-export default function HomeScreen() {
-  return (
-    <SafeAreaView   className= "flex-1 justify-center items-center">
-    <View className="  bg-blue-500">
-   < Text className={"text-red-600 "}>Hello NativeWind!</Text>
-    </View>
+export default function PageRoot () {
 
-   </SafeAreaView>
+ //return <Redirect href="/(root)/(tabs)/home" />;
 
-  );
-}
-
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
-// //
+  return <Redirect href="/(auth)/welcome" />;
+};
